c6fd9c223295277b5b49beb387c72e5afc6417ad
bun <vlongbf@gmail.com>
2025-01-03 13:50:39 +0900

    改善

--- mypkg/batterytalker.py
+++ mypkg/batterytalker.py
@@ -1,19 +1,21 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int32
+from std_msgs.msg import Int16
 import psutil
 
 class BatteryTalker(Node):
     def __init__(self):
         super().__init__('batterytalker')
-        self.pub = self.create_publisher(Int32, 'battery_status', 10)
+        self.pub = self.create_publisher(Int16, 'battery_status', 10)
         self.create_timer(1.0, self.cb)
-        
+        self.count = 0
+
     def cb(self):
         battery = psutil.sensors_battery()
         percent = int(battery.percent) if battery else 0
-        self.get_logger().info(f"Battery level: {percent}%")
-        msg = Int32(data=percent)
+        self.count += 1
+        self.get_logger().info(f"Battery level: {percent}% - Count: {self.count}")
+        msg = Int16(data=percent)
         self.pub.publish(msg)
 
 def main():
